27da2a4e84dfa4fdd6f57d7cbc1acd6f60153a39
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-19 13:46:32 +0200

    Stop tracking final directory

---
+++ starter/event-loop.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+
+fs.readFile('./test-file.txt','utf-8',(err)=>{
+
+  console.log('It finished loading I/O');
+
+  setTimeout(()=>{console.log('Timeout 0.5 seconds');},300);
+  setTimeout(()=>{console.log('TImeout 0 seconds');},0);
+  setImmediate(()=>{console.log('Set immediate');});
+});
+fs.readFile('./test-file.txt','utf-8',(err)=>{
+
+  console.log('It finished loading I/O second');
+
+});
+fs.readFile('./test-file.txt','utf-8',(err)=>{
+
+  console.log('It finished loading I/O third');
+
+});
+fs.readFile('./test-file.txt','utf-8',(err)=>{
+
+  console.log('It finished loading I/O  fourth');
+
+});
+console.log('Top lvl code!');
